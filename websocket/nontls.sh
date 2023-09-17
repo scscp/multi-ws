e1744b4c44cbe8628dbd35ba9e681ae716099db5
scscp <143437421+scscp@users.noreply.github.com>
2023-09-17 23:42:24 +0700

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -16,12 +16,12 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Installing SSH Websocket None TLS by YasaNata" | lolcat
+echo "Installing SSH Websocket None TLS by memek" | lolcat
 echo "Progress..." | lolcat
 sleep 3
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/yasanata/multi-ws/main/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/scscp/multi-ws/main/websocket/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
